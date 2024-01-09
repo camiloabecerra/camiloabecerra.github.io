4239d446f15ad0d8ce43414c2fbbc6f0697a7b9a
Camilo Becerra <colcan7878@gmail.com>
2024-01-09 14:31:59 -0500

    style fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".menu-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".menu-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
